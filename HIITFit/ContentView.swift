4fc88944149fb4468edeafba688bd0d99b72041f
Karol Moroz <k.j.moroz@gmail.com>
2022-05-26 12:44:33 +0200

    Add tab view, format swift files

--- HIITFit/ContentView.swift
+++ HIITFit/ContentView.swift
@@ -33,14 +33,18 @@
 import SwiftUI
 
 struct ContentView: View {
-  var body: some View {
-    Text("Hello, world!")
-      .padding()
-  }
+    var body: some View {
+        TabView {
+            Text("Welcome").tabItem { Text("Welcome") }
+            Text("Exercise 1").tabItem { Text("Exercise 1") }
+            Text("Exercise 2").tabItem { Text("Exercise 2") }
+        }.tabViewStyle(PageTabViewStyle())
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
+    static var previews: some View {
+        ContentView()
+    }
 }
