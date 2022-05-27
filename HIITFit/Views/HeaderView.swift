06417e2e955125fbc3d1383c157af86239595f4b
Karol Moroz <karol@moroz.dev>
2022-05-27 20:48:49 +0200

    Build and refactor out HeaderView

--- HIITFit/Views/HeaderView.swift
+++ HIITFit/Views/HeaderView.swift
@@ -32,26 +32,32 @@
 
 import SwiftUI
 
-struct ExerciseView: View {
-    let videoNames = ["squat", "step-up", "burpee", "sun-salute"]
-    let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
-
-    let index: Int
+struct HeaderView: View {
+    let exerciseName: String
 
     var body: some View {
         VStack {
-            HeaderView(exerciseName: exerciseNames[index])
-            Text("Video player")
-            Text("Timer")
-            Text("Start/Done button")
-            Text("Rating")
-            Text("History button")
+            Text(exerciseName)
+                .font(.title)
+            HStack {
+                ForEach(0 ..< 4) { i in
+                    Image(systemName: "\(i + 1).circle")
+                }
+            }
+            .font(.title2)
         }
     }
 }
 
-struct ExerciseView_Previews: PreviewProvider {
+struct HeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        ExerciseView(index: 0)
+        Group {
+            HeaderView(exerciseName: "Test")
+                .previewLayout(.sizeThatFits)
+            HeaderView(exerciseName: "Test")
+                .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.dark)
+                .environment(\.sizeCategory, .accessibilityLarge)
+        }
     }
 }
